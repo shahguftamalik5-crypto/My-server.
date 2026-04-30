b1b23bfec326d9fe9237f64bb25eb71c625c99fc
shahguftamalik5-crypto <shahguftamalik5@gmail.com>
2026-04-30 15:52:05 +0530

    Update response message to HTML format

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Minecraft Panel is Online!');
+  res.send('<h1>Minecraft Panel is Online!</h1><p>Server is running perfectly.</p>');
 });
 
 app.listen(port, () => {
