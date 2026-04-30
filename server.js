5b54664c08115fb46ce42a1574ef2571fff3efc8
shahguftamalik5-crypto <shahguftamalik5@gmail.com>
2026-04-30 16:14:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const path = require('path');
+const path = require('path'); // Ye line zaroori hai
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Ye batata hai ki jab koi link khole toh index.html dikhao
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
